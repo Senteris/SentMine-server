f70df332aa849785a45e769e10bf921c03a6d7fd
Xefise <Xefise@gmail.com>
2020-06-06 20:07:33 +0300

    Remove zombiespiders

--- server/ServerStart.bat
+++ server/ServerStart.bat
@@ -3,7 +3,7 @@
 call settings.bat
 
 :start_server
-echo Starting SevTech Ages Server...
+echo Starting SentMinePack server
 java -server -Xms%MIN_RAM% -Xmx%MAX_RAM% %JAVA_PARAMETERS% -jar %SERVER_JAR% nogui
 pause
 exit /B
